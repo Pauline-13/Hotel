9df313154677901a0a2c356797def115860cc322
bryanT062 <bryantate899@gmail.com>
2025-05-20 15:47:23 +0200

    20/05/25

--- js/hebergement.js
+++ js/hebergement.js
@@ -1,34 +1,57 @@
-const sectionElement = document.querySelector("section");
-let resultsArray;
+const sectionElement = document.getElementById("ici");
+let resultsIds = [];
+let index = 0;
 
 fetch("../Json/hebergement.json")
   .then((response) => response.json())
   .then((json) => {
-    console.log(json);
     resultsIds = json.ids;
-    afficheur(resultsIds);
+    initialiserStructure();
+    afficherChambre(index); 
   });
 
-function afficheur(ids) {
+function initialiserStructure() {
+  const containerChambre = document.createElement("div");
+  containerChambre.id = "chambre-container";
+  sectionElement.appendChild(containerChambre);
+
   
+  const btnGauche = document.createElement("button");
+  
+  btnGauche.innerHTML= `
+ <i class="fa-solid fa-arrow-left"></i>`
+  btnGauche.classList.add("fleche-gauche");
+  btnGauche.addEventListener("click", () => {
+    index = (index - 1 + resultsIds.length) % resultsIds.length;
+    afficherChambre(index);
+  });
+  sectionElement.appendChild(btnGauche);
 
-  for (const id of ids) {
-    const chambre = id.nom;
-    const image = id.image;
-    const info = id.description;
-
-    const divElement = document.createElement("div");
-    sectionElement.appendChild(divElement);
-    divElement.classList.add("chambre");
-
-    divElement.innerHTML = `
-      
-        <div>
-           <img src="${image}" alt="${chambre}">
-           <p><strong>${chambre}</strong></p>
-           <p>${info}</p>
-        </div>
-      
-    `;
-  }
+  const btnDroite = document.createElement("button");
+ 
+  btnDroite.innerHTML= `
+  <i class="fa-solid fa-arrow-right"></i>`
+  btnDroite.classList.add("fleche-droite");
+  btnDroite.addEventListener("click", () => {
+    index = (index + 1) % resultsIds.length;
+    afficherChambre(index);
+  });
+  sectionElement.appendChild(btnDroite);
 }
+
+function afficherChambre(i) {
+  const chambre = resultsIds[i];
+  const container = document.getElementById("chambre-container");
+
+  container.innerHTML = `
+    
+      <img id="image" src="${chambre.image}" alt="">
+
+      <div class="p">
+        
+        <h1><strong>${chambre.nom}</strong></h1>
+        <p>${chambre.description}</p>
+       
+      </div>`;
+}
+
